59ac14a32144ff36843b0f1761fad98eae5075af
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-03-11 23:58:21 +0100

    restTemplate to rest_application added

--- src/main/java/microservices/rest_application/Config.java
+++ src/main/java/microservices/rest_application/Config.java
@@ -1,5 +1,6 @@
 package microservices.rest_application;
 
 public class Config {
-    public static String port = "http://localhost:4000/";
+//    public static String port = "http://localhost:4000/";
+    public static String port = "http://172.19.117.145:4000/";
 }
